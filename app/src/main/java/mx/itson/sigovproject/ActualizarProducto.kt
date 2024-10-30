cf3d06b16c31a76c11c051f58f4f0336f07c158e
Jorge Estrada <jorgeaestrada45@gmail.com>
2024-10-29 23:29:22 -0700

    Caso Actualizar Producto

--- app/src/main/java/mx/itson/sigovproject/ActualizarProducto.kt
+++ app/src/main/java/mx/itson/sigovproject/ActualizarProducto.kt
@@ -1,19 +1,19 @@
 package mx.itson.sigovproject
 
-import android.R
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-
-class RegistrarProducto : AppCompatActivity() {
-
+class ActualizarProducto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(mx.itson.sigovproject.R.layout.activity_registrar_producto)
-        val btnRegistrar: Button = findViewById(mx.itson.sigovproject.R.id.btn_registrar)
+        setContentView(R.layout.activity_actualizar_producto)
+        val btnRegistrar: Button = findViewById(mx.itson.sigovproject.R.id.btn_consultar)
         btnRegistrar.setOnClickListener {
             mostrarDialogoConfirmacion()
         }
@@ -21,8 +21,8 @@ class RegistrarProducto : AppCompatActivity() {
 
     private fun mostrarDialogoConfirmacion() {
         AlertDialog.Builder(this)
-            .setTitle("Confirmacion de Registro")
-            .setMessage("¿Deseas Registrar El Producto?")
+            .setTitle("Confirmacion de Actualizacion")
+            .setMessage("¿Deseas Actualizar El Producto?")
             .setPositiveButton("CONFIRMAR") { dialog, _ ->
                 Toast.makeText(this, "Producto registrado exitosamente", Toast.LENGTH_SHORT).show()
                 dialog.dismiss()
